5fb8e21fff318c20b6b4a811882a6f34bb7e295c
rizwan ishaq <rizwanishaq@gmail.com>
2024-06-05 17:32:42 +0200

    next commit

--- components/dashboard/AnalyticsChart.jsx
+++ components/dashboard/AnalyticsChart.jsx
@@ -1,10 +1,9 @@
 "use client";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from "recharts";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, ResponsiveContainer, Tooltip, Legend } from "recharts";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -24,10 +23,13 @@ const AnalyticsChart = () => {
             <div style={{ width: "100%", height: 300 }}>
             <ResponsiveContainer>
                 <LineChart width={1100} height={300} data={data}>
-                    <Line type='monotone' dataKey='uv' stroke="#8884d8"/>
+                    <Line type='monotone' dataKey='uv' stroke="#8884d8" activeDot={{ r: 8 }}/>
+                    <Line type='monotone' dataKey='pv' stroke="#82ca9d"/>
                     <CartesianGrid stroke="#ccc" />
                     <XAxis dataKey='name'/>
                     <YAxis />
+                    <Tooltip />
+          <Legend />
                 </LineChart>
             </ResponsiveContainer>
             </div>
--- data/analytics.js
+++ data/analytics.js
@@ -5,72 +5,84 @@ const data = [
     uv: 4000,
     pv: 2400,
     amt: 2400,
+    color: 'red',
   },
   {
     name: 'Feb',
     uv: 3000,
     pv: 1398,
     amt: 2210,
+    color: 'green'
   },
   {
     name: 'Mar',
     uv: 2000,
     pv: 9800,
     amt: 2290,
+    color: 'blue'
   },
   {
     name: 'Apr',
     uv: 2780,
     pv: 3908,
     amt: 2000,
+    color: 'yellow'
   },
   {
     name: 'May',
     uv: 1890,
     pv: 4800,
     amt: 2181,
+    color: 'pink'
   },
   {
     name: 'June',
     uv: 2390,
     pv: 3800,
     amt: 2500,
+    color: 'purple'
   },
   {
     name: 'July',
     uv: 3490,
     pv: 4300,
     amt: 2100,
+    color: 'orange'
   },
   {
     name: 'Aug',
     uv: 2400,
     pv: 2400,
     amt: 2400,
+    color: 'red',
   },
   {
     name: 'Sep',
     uv: 3500,
     pv: 1300,
     amt: 2210,
+    color: 'green'
   },
   {
     name: 'Oct',
     uv: 3000,
     pv: 9800,
     amt: 2290,
+    color: 'blue'
   },
   {
     name: 'Nov',
     uv: 2000,
     pv: 3908,
     amt: 2000,
+    color: 'yellow'
   },
   {
     name: 'Dec',
     uv: 1890,
     pv: 4800,
     amt: 2181,
+    color: 'pink'
   },
 ];
 
